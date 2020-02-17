04ed5f1b48cb8ea0fbf55cf0668b9e2beea88717
kevin <kevinothira@gmail.com>
2020-02-17 12:02:54 +0300

    added akan names and days of the week

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var akanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+var akanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+var dayOfTheWeek =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
